c830fa596d9b5578603d01e531fd3f724c84ed1a
ellyromano03 <37980524+ellyromano03@users.noreply.github.com>
2021-04-12 09:13:29 +0200

    Add files via upload

---
+++ esercizio 1.js
@@ -0,0 +1,4 @@
+//es 1: Data una lista di 500 numeri casuali, restituirne una con gli elementi invertiti di posizione, dall’ultimo al primo (es. [1, 2, 3] > [3, 2, 1])
+
+const data = es.data
+let result = data.reverse()
